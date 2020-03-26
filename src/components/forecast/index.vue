c7785726a5714c03e4e5ddacc55702757f41110c
JeffWu <isjeffcom@gmail.com>
2020-03-26 16:53:06 +0000

    init forecast

---
+++ src/components/forecast/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id='forecast'>
+        <apexchart width="100%" height="320px" type="area" :options="chartOptions" :series="chartData_tested"></apexchart>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    name: "forecast",
+    props:{
+        uk: {
+            type: Array,
+            default(){
+                return []
+            }
+        },
+
+        italy: {
+            type: Array,
+            default(){
+                return []
+            }
+        }
+    },
+
+    methods:{
+        
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
